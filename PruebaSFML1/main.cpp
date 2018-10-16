04f1fbf28ee8a05f89a99c8319892428e275fe82
Abraham Armas Cordero <abraham.armas@enti.cat>
2018-10-16 11:27:41 +0200

    Estructura de funcionamiento

--- PruebaSFML1/main.cpp
+++ PruebaSFML1/main.cpp
@@ -1,47 +1,65 @@
 #include <iostream>
-#include <SFML/Graphics.hpp>
+
+//temario
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/wait.h>
+#include <wait.h>
+//
 #include <vector>
 #include "GraficoSFML.h"
+#include <SFML/Graphics.hpp>
 
+//constantes
 #define WINDOW_H 800
 #define WINDOW_V 600
 #define TITLE "Mi super practica 1.1"
-
-GraficoSFML graficos;
-
-void ClockAlarm(int param);
-
-static int fdP1[2]; // 0 lectura, 1escritura
-static int fdP2[2];
+static int fdS0[2]; // 0 lectura, 1escritura
+static int fdS1[2];
 pid_t son1;
 pid_t son0;
 
+GraficoSFML graficos;
+// --- FUNCIONES
+void ClockAlarm(int param);
+//se ejecuta cuando el hijo 0 recibe un SIGALRM (cada 5 segundos)
+void CargarCliente(int param);
+//se ejecuta cuando el hijo 2 recibe un SIGUSR2
+void UnLoadClient(int param);
+
+//codigo string de los clientes "color,sitio"
+
 int main()
 {
-    int statusPipeS1 = pipe(fdP1);
-    if (statusPipeS1 <0) throw "error en pipe 1";
-
-    int statusPipeS2 = pipe(fdP2);
-    if (statusPipeS2 <0) throw "error en pipe 2";
-
+    int statusPipeS0 = pipe(fdS0);
+    if (statusPipeS0 <0) throw "error en pipe 1";
 
     //llegada de clientes
-    if(son1 = fork() == 0){
+    if(son0 = fork() == 0){
         //escribe que cliente hay que dibujar
+        while(1){
+        //mirar el fdS0 a ver si hay sitio libre
+        //if yes
+            signal(SIGALRM, CargarCliente);
+        //else
+            pause();
+        }
+        exit(0);
+
     }else {
     //control de vaciado de clientes
-        if(son2 = fork()== 0){
+        int statusPipeS1 = pipe(fdS1);
+        if (statusPipeS1 <0) throw "error en pipe 2";
+        if(son1 = fork()== 0){
 
         }
         else{
+            sf::RenderWindow window(sf::VideoMode(WINDOW_H,WINDOW_V), TITLE);
+            signal(SIGALRM,ClockAlarm);
+            alarm(1);
 
             while(window.isOpen())
             {
-                sf::RenderWindow window(sf::VideoMode(WINDOW_H,WINDOW_V), TITLE);
-                signal(SIGALRM,ClockAlarm);
-                alarm(1);
                 sf::Event event;
                 while(window.pollEvent(event))
                 {
@@ -102,6 +120,9 @@ int main()
                 if (graficos.tiempoRestante <= 0)
                 window.close();
             }
+            //esperar a todos los hijos
+            //matarlos
+            //exit
         }
     }
     return 0;
@@ -111,4 +132,16 @@ void ClockAlarm(int param)
 {
     graficos.tiempoRestante--;
     alarm(1);
+}
+
+void CargarCliente(int param){
+//Acceso a los clientes predeterminados
+//vaciar el fdS0 y escribir la info del nuevo cliente
+//mandar SIGUSR1 al padre
+}
+
+void UnLoadClient(int param){
+//alarm(5)
+//mandar SIGUSR2 al padre
+
 }

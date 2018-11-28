daef153c287465c70a56ebd5bb70235d90b2a691
Haardrade <maurizio.carlotta@enti.cat>
2018-11-28 19:50:52 +0100

    Network linked

--- MazeGo/PruebaSFML1/include/GraficoSFML.h
+++ MazeGo/PruebaSFML1/include/GraficoSFML.h
@@ -21,59 +21,6 @@
 
 class GraficoSFML
 {
-    public:
-        sf::CircleShape jugador;
-        sf::RectangleShape manoIzquierda;
-        sf::RectangleShape manoDerecha;
-
-        std::vector<sf::RectangleShape> aObjetosADibujar;
-
-        std::vector<sf::Text> aTextosADibujar;
-        sf::CircleShape aTaburetesADibujar[NUM_MESAS];
-        sf::RectangleShape aPedidosADibujar[NUM_MESAS];
-
-        int tiempoRestante;
-        int numClientesRestantes;
-        sf::Vector2f posicionJugador;
-
-        GraficoSFML();
-        bool TabureteComiendo(int posicion_);
-        bool TabureteVacio(int _posicion);
-        bool TabureteOcupado(int);
-        void OcupaTaburete(int _posicion);
-        void VaciaTaburete(int _posicion);
-
-        bool RestauranteLLeno();
-        void VaciaPedido(int _posicion);
-        void PonPedido(int _posicion, sf::Color _queComida);
-
-        void CogeComida(sf::Color _queComida);
-
-        //Retorna false si la comida que tiene que dejar no la tiene en ninguna de las dos manos.
-        bool DejaComida(sf::Color _queComida);
-        void TiraComida();
-
-        void MueveJugador(sf::Vector2f _posicion);
-        virtual ~GraficoSFML();
-
-         void UpdateTimer();
-         void UpdateClientCounter();
-
-    protected:
-
-    private:
-
-        sf::Font font;
-        std::vector<sf::Vector2f> aOrigenMesas;
-        std::vector<sf::Vector2f> aOrigenDispensadores;
-        void InitContadorTiempo();
-        void InitContadorClientes();
-        void InitTaburetes();
-        void InitPedidos();
-        void InitObjetosFijos();
-        void InitPlayer();
-        void UpdatePlayer();
-
 };
 
 #endif // GRAFICOSFML_H
